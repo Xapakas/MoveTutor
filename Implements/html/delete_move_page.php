81199bb84bee5a80bc2732ed4954c7d982dc9c75
noah.feld <noah.feld@centre.edu>
2020-10-07 20:20:05 +0000

    Created delete_move_page.php and made minor html updates to update_move_page.php

--- Implements/html/delete_move_page.php
+++ Implements/html/delete_move_page.php
@@ -1,28 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Update a Move</title>
+    <title>Delete a Move</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
 <?php
 
-/* Show all PHP errors. */
-
+// Show all PHP errors.
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-/* Set connection info. */
 
 $dbhost = 'localhost';
 $dbuser = 'webuser';
 $dbpass = 'pass';
 $dbname = 'move_tutor';
 
-/* Make connection, and return an error if it fails. */
-
+/* Make connection, and return error if it fails. */
 if (!$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname)){
     echo "Error: Failed to make a MySQL connection: " . "<br>";
     echo "Errno: $conn->connect_errno; i.e. $conn->connect_error \n";
@@ -42,12 +38,11 @@ if (!$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname)){
 <div class="main">
 
 <div class="header">
-    <h2>Select a move and update its time (past, present, or future).</h2>
+    <h2>Select a move by name to delete it.</h2>
 </div>
 
 <div class="contents">
-
-<form action="update_move_page.php" method="post">
+<form action="delete_move_page.php" method="post">
 
     <label for="move_name">Select move by name:</label><br>
     <input list="names" name="move_name" id="move_name">
@@ -67,45 +62,31 @@ if (!$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname)){
     ?>
     </datalist>
 
-    <p>Change the move time to...</p>
-
-    <input type="radio" id="past" name="move_time" value="past">
-    <label for="past">Past</label><br>
-
-    <input type="radio" id="present" name="move_time" value="present">
-    <label for="present">Present</label><br>
-
-    <input type="radio" id="future" name="move_time" value="future">
-    <label for="future">Future</label><br>
-
-    <input type="radio" id="N/A" name="move_time" value="N/A">
-    <label for="N/A">N/A</label><br>
-
     <input type="submit" value="Submit">
 
 </form>
-
 </div>
+
 </div>
 
 <?php
 
-    if (isset($_POST["move_name"]) && isset($_POST["move_time"])){
-        // prepared statement, of course.
-        $updt_stmt = $conn->prepare("UPDATE moves SET move_time = ? WHERE move_name = ?;");
-        $updt_stmt->bind_param('ss', $_POST["move_time"], $_POST["move_name"]);
-        // now, actually update.
-        if (!$updt_stmt->execute()){ 
-            echo $conn->error;
-            echo "\n Update query failed!";
-        } 
-        else { // redirect so refreshing doesn't break everything.
-            header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
-            exit();
-        }
+if (isset($_POST["move_name"])){
+    // prepared statement, obviously.
+    $del_stmt = $conn->prepare("DELETE FROM moves WHERE move_name = ?;");
+    $del_stmt->bind_param('s',$_POST["move_name"]);
+    // now, for the deletion.
+    if (!$del_stmt->execute()){ 
+        echo $conn->error;
+        echo "\n Deletion query failed!";
     }
+    else { // redirect, so refreshing doesn't break everything.
+        header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+        exit();
+    }
+}
 
-    $conn->close();
+$conn->close();
 
 ?>
 
